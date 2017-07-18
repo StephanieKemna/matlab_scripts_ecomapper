4ec1fec603c4fa1b8c010dac91c0bf3f14cb814b
Jessica Gonzalez <6518317+StephanieKemna@users.noreply.github.com>
2017-07-18 11:20:46 -0700

    Updated scripts to allow the computer to run it faster. Updated the extracting columns and stored data as .mat

--- LogFileDataImport.m
+++ LogFileDataImport.m
@@ -12,60 +12,50 @@ run em_prepare_labels
 [filename, Pathname] = uigetfile('*.log', 'Select the data file', ... 
    '/home/jessica/data_em/Logs/puddingstone_20170627/20170627_193227_UTC_0_jessica_mission1_IVER2-135.log');
 file_path = strcat(Pathname, filename);
-all_data = csvimport(file_path, 'delimiter',';'); % Places all data set into large cell
 
-% Extract the first row from the file
-first_row = (all_data(1,:));
+% Changing the .log file into .mat, while specifying data_type 
+filename_mat = strcat(Pathname, filename(1:length(filename)-4), '_', data_type, '.mat');
 
-% Create a string for Latitude 
-latitude_index = find_index(first_row, 'Latitude'); 
+if exist(filename_mat, 'file') == 0
+    %Extracting data from each column
+    [latitude_column, longitude_column, depth_column, generic_column]= ...
+        csvimport(file_path, 'columns', {'Latitude', 'Longitude', 'DFS Depth (m)', ...
+        type_string},'delimiter',';');
 
-% Create string for Longitude
-longitude_index = find_index(first_row, 'Longitude');
+    % Depth min and max
+    min_dep = min(depth_column);
+    max_dep = max(depth_column);
 
-% Create string for generic data (sensors_cell options)
-generic_index = find_index(first_row,type_string); 
+    % Range depth
+    dep_range = max_dep - min_dep;
+    num_steps = 100;
 
-% Create string for Depth
-depth_index = find_index(first_row, 'DFS Depth (m)'); 
+    % Step size of depth
+    dep_step_size = dep_range/num_steps;
 
+    % ProducesnNumber of copies for Lat & Long (so hardcoding isn't necessary)
+    depth_range = min_dep : dep_step_size : max_dep;
+    depth_length = length(depth_range);
 
+    % Produces the number of copies for Depth
+    longitude_length = length(longitude_column);
 
-% Extracts data from specific column from file/cell
-latitude_column = [all_data{2:size(all_data,1),latitude_index}];
+    % Variables necessary for griddata so a cube of informaton isn't produced
+    lon_copies = repmat(longitude_column', depth_length, 1);
+    lat_copies = repmat(latitude_column', depth_length, 1);
+    depth_copies= repmat(depth_range', 1, longitude_length);
 
-longitude_column = [all_data{2:size(all_data,1),longitude_index}];
+    % Creating the grid and the values for each axis
+    grid_data_values= griddata(longitude_column, latitude_column, depth_column,...
+        generic_column, lon_copies, lat_copies, depth_copies);
+    
+    % Saving the filename_mat with variables
+    save(filename_mat, 'lon_copies', 'lat_copies', 'depth_copies', 'grid_data_values');
 
-depth_column = [all_data{2:size(all_data,1),depth_index}];
+else 
+    load(filename_mat)
 
-generic_column = [all_data{2:size(all_data,1),generic_index}];
-
-% Depth min and max
-min_dep = min(depth_column);
-max_dep = max(depth_column);
-
-% Range depth
-dep_range = max_dep - min_dep;
-num_steps = 100;
-
-% Step size of depth
-dep_step_size = dep_range/num_steps;
-
-% ProducesnNumber of copies for Lat & Long (so hardcoding isn't necessary)
-depth_range = min_dep : dep_step_size : max_dep;
-depth_length = length(depth_range);
-
-% Produces the number of copies for Depth
-longitude_length = length(longitude_column);
- 
-% Variables necessary for griddata so a cube of informaton isn't produced
-lon_copies = repmat(longitude_column, depth_length, 1);
-lat_copies = repmat(latitude_column, depth_length, 1);
-depth_copies= repmat(depth_range', 1, longitude_length);
-
-% Creating the grid and the values for each axis
-grid_data_values= griddata(longitude_column, latitude_column, depth_column,...
-    generic_column, lon_copies, lat_copies, depth_copies);
+end
 
 % Plot the completed 3D slice/graph
 three_dimensional_slice = surf(lon_copies, lat_copies, -depth_copies, grid_data_values);
