acfd76463488a67bfd30ae7c6c3511ef9775d979
Stephanie Kemna <6518317+StephanieKemna@users.noreply.github.com>
2017-08-13 16:18:19 -0700

    uigetdir for user friendliness

--- compile_all.m
+++ compile_all.m
@@ -3,31 +3,30 @@
 %
 %  data_path_prefix: no default; define where to search for log files
 %                   (day/mission folder)
-%  b_localtime: convert UTC to local time? (0 or 1, default: 0)
-%  b_dst: use Daylight Savings Time (if b_localtime)? (0 or 1, default: 0)
+%  b_localtime: convert UTC to local time? (0 or 1, default: 1)
+%  b_dst: use Daylight Savings Time (if b_localtime)? (0 or 1, default: 1)
 %  multiple_folders: find log files in multiple mission folders? (0 or 1, default: 0)
 %  location: experiment location, default: 'puddingstone'
 %
 %
 % Author: Stephanie Kemna
 % Institution: University of Southern California
-% Date: Spring 2016
+% Date: Spring 2016 - Summer 2017
 %
-% tested with MatlabR2012a on Ubuntu 14.04
+% tested with MatlabR2012a on Ubuntu 16.04
 %
 function [] = compile_all(data_path_prefix, b_localtime, b_dst, multiple_folders, location)
 
 %% input / preparation
 if nargin < 1
-  disp('ERROR: no input arguments. Options are:')
-  disp('       data_path_prefix, b_localtime, b_dst, multiple_folders, location');
-  return
+  % if no folder specified, ask the user
+  data_path_prefix = uigetdir;
 end
 if nargin < 2
-  b_localtime = 0;
+  b_localtime = 1;
 end
 if nargin < 3
-  b_dst = 0;
+  b_dst = 1;
 end
 if nargin < 4
   multiple_folders = 0;
