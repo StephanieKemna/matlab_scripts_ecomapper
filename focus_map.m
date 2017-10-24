616d156bedffb2cf79dfc87c91235c7985462abd
Stephanie Kemna <6518317+StephanieKemna@users.noreply.github.com>
2017-10-24 15:41:41 -0700

    add scmi

--- focus_map.m
+++ focus_map.m
@@ -1,6 +1,7 @@
-if ( strcmp(location,'puddingstone') == 1 )
+if ( strcmp(location,'puddingstone') )
   axis([-117.813 -117.806 34.0864 34.0912])
-elseif ( strcmp(location,'elsinore') == 1 )
+elseif ( strcmp(location,'elsinore') )
   axis([-117.38 -117.35 33.662 33.68])
+elseif ( strcmp(location,'scmi') )
+  axis([-118.271 -118.265 33.735 33.738])
 end
-
